45dee3e0b0acfb303cefb549978adfde612d0bf0
Haruka Matsumoto <matsumoto.haru@gmail.com>
2017-12-19 09:40:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,11 @@ function logWhisper (str) {
   console.log(str.toLowerCase())
 }
 function sayHiToGrandma (str) {
-  if (str.isLowerCase()) {
-    
+  if (str === "I love you, Grandma.") {
+    return "I love you, too."
+  } else if (str.toLowerCase() === str) {
+    return "I can't hear you!"
+  } else if (str.toUpperCase() === str) {
+    return "YES INDEED!"
   }
 }
